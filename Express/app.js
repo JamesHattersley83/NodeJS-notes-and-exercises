cd32dba99ea44a704c4c382babd345ecd49068b3
James Hattersley <jameshattersley83@gmail.com>
2019-07-09 22:04:55 +0100

    added template engine and querystring exercises

--- Express/app.js
+++ Express/app.js
@@ -1,24 +1,43 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
 // middleware
 app.use("/assets", express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
-  res.send(
-    "<html><head><link href='assets/style.css' type='text/css' rel='stylesheet'/></head><body><h1>Hello World</h1></body></html>"
-  );
-});
+app.set("view engine", "ejs");
 
-app.get("/", function(req, res, next) {
+app.use("/", function(req, res, next) {
   console.log("Request Url:" + req.url);
   next();
 });
 
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
 app.get("/person/:id", function(req, res) {
-  res.send("Person: " + req.params.id);
+  res.render("Person", { ID: req.params.id, Qstr: req.query.qstr });
+});
+
+app.post("/person", urlencodedParser, function(req, res) {
+  res.send("Thank you!");
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+});
+
+app.post("/personjson", jsonParser, function(req, res) {
+  res.send("Thank you for the JSON data!");
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
 });
 
 app.get("/api", function(req, res) {
